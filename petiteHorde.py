58514789180281026a6526d02d174d6652b07f5a
Manon <smequeuse@yahoo.fr>
2015-10-22 20:59:57 +0200

    expectations

--- petiteHorde.py
+++ petiteHorde.py
@@ -14,12 +14,59 @@ class petitTerrain:
         self.content =[["plaine"]*(2*taille + 1)]*(2*taille + 1)
 
         
+class petiteAction:
+    def __init__(self, name):
+        self.name = name
+        self.needs = {}
+        self.results = {}
+        
+    def expects(self, objects):
+        distanceRate = 0.01
+        totalFactor = 1.
+        for o in self.needs.keys():
+            quantity = 0
+            if o in objects.keys():
+                quantity = objects[o]
+            totalFactor *= 1 - distanceRate*abs(quantity - self.needs[o])
+            
+        res = {}
+        for r in self.results.keys():
+            res[r] = totalFactor*self.results[r]
+
+        return res
+    
+    def feedback(self, objects, results):
+        rate = 0.01
+        for o in self.needs.keys():
+            self.needs[o]*=(1 - rate)
+            
+        for o in objects.keys():
+            if o not in self.needs.keys():
+                self.needs[o] = rate*objects[o]
+                
+            else:
+                self.needs[o] += rate*objects[o]
+                
+        for r in self.results.keys():
+            self.results[r] *= (1 - rate)
+            
+        for r in results.keys():
+            if r not in self.results.keys():
+                self.results[r] = rate*results[r]
+            else:
+                self.results[r] += rate*results[r]
+                
+
+
+        
+        
 consonnes_rares = "B.D.F.G.H.J.V.Qu.Ch.Pr.Cr.Sc"
 consonnes_frequentes = "C.L.M.N.P.R.S.T.St.Tr"
 consonnes = consonnes_frequentes.split('.')*2 + consonnes_rares.split('.')
 voyelles_rares = "u.in.ai.ei.eu.ia.ui.io"
 voyelles_frequentes = "a.e.i.o.on.ou.en.au"
 voyelles = voyelles_frequentes.split('.')*2 + voyelles_rares.split('.')    
+
         
 class petitHomme:
     def __init__(self, horde):
@@ -36,11 +83,13 @@ class petitHomme:
             self.opinions[p.name]  = 1
             p.opinions[self.name] = 1
             
+        self.knownActions = {}
+            
         self.age = 0
         self.sante = 1.
         self.forme = 1.
         
-        self.action = "rester"
+        self.action = "cueillir"
         self.group = None
         self.oldReserve = self.horde.reserve
         
@@ -55,6 +104,31 @@ class petitHomme:
         self.selectNextAction()
         
     def resolveAction(self):
+        result = {}
+        mangerTotal = 0.
+        if self.action == "cueillir":
+            result["baies"] = 0.6*(random.random()+random.random()) + 0.2
+            mangerTotal += result["baies"]
+        #~ else:
+            #~ result = 0.
+        sparseLogs(self.name, self.name+ " brings from "+self.action+": "+str(result))
+        self.horde.reserve += mangerTotal
+        mange = min(self.horde.reserve/len(self.horde.personnes.keys()), 1.)
+        self.horde.reserve -= mange
+        sparseLogs(self.name, self.name+ "mange "+str(mange))
+        self.horde.changeOpinion(self.name, mangerTotal - mange )
+        self.opinions[self.name] +=  mangerTotal - mange
+        
+        if self.action not in self.knownActions.keys():
+            self.knownActions[self.action] = petiteAction(self.action)
+            
+        self.knownActions[self.action].feedback({}, result)
+        sparseLogs(self.name, self.name+" action "+self.action)
+        sparseLogs(self.name, self.name+" needs "+str(self.knownActions[self.action].needs))
+        sparseLogs(self.name, self.name+" action "+str(self.knownActions[self.action].results))
+        
+        
+    def resolveAction_old(self):
         
         if self.action == "cueillir":
             result = 0.6*(random.random()+random.random()) + 0.2
@@ -89,6 +163,45 @@ class petitHomme:
             
         
     def selectNextAction(self):
+        hope = self.horde.getHope(self.name)
+        sparseLogs(self.name, self.name+" hope "+str(hope))
+ 
+        
+        self.action = ""
+        
+        if random.randint(0, 10) == 0:
+            actions = ["cueillir", "rester"] + self.knownActions.keys()
+            self.action = random.choice(actions)
+            sparseLogs(self.name,self.name+ ": random chosen action "+self.action)
+            return
+
+        
+        minDist = 10.
+        
+        for a in self.knownActions.values():
+            resA = a.expects({})
+            sparseLogs(self.name,self.name+ " expects from "+a.name+" "+str(resA))
+            if "baies" in resA:
+                dist = abs(resA["baies"] - hope)
+            else:
+                dist = max(0, hope)
+                
+            if dist < minDist:
+                minDist = dist
+                self.action = a.name
+             
+        sparseLogs(self.name, minDist)          
+        
+            
+        if self.action is "":
+            
+            actions = ["cueillir", "rester"] + self.knownActions.keys()
+            self.action = random.choice(actions)
+            
+        sparseLogs(self.name,self.name+ ": chosen action "+self.action)
+        
+        
+    def selectNextAction_old(self):
         hope = self.horde.getHope(self.name)
         sparseLogs(self.name, self.name+" hope "+str(hope))
         
@@ -180,6 +293,11 @@ class petiteHorde:
             
         self.groupActivities = []
         
+        self.objects = {}
+        self.objects["bout de bois"] = 4
+        self.objects["caillou pour taper"] = 3
+        self.objects["caillou pour lancer"] = 10
+        
             
     def addPersonne(self):
         homme = petitHomme(self)
@@ -223,3 +341,11 @@ for i in range(10):
     print "---"
     pH.update()
     
+#~ a = petiteAction("test")
+#~ a.feedback({"lance":1, "corde":4}, {"viande":2, "cuir":1})
+#~ a.feedback({"lance":1, "corde":4}, {"viande":2, "cuir":1})
+#~ a.feedback({"lance":1, "corde":4}, {"viande":2, "cuir":1})
+#~ print a.needs
+#~ print a.results
+    
+#~ print a.expects({"corde":2, "massue":1})
